a4a14bfb2c50b413bb427d615e04b92e03510fe9
Allison Hemzacek <allison.hemzacek@gmail.com>
2022-05-31 20:42:32 -0600

    final draft

---
+++ arraysCFU.rb
@@ -0,0 +1,12 @@
+animals = ["pig", "cat", "bird", "dog"]
+lucky_numbers = [7, 10, 99, 51]
+tax_returns = [250.43, 378.02, 100.50, 543.99]
+full_time_student = [true, false, false, true]
+# removes last array
+animals.pop
+# adds to end of array
+lucky_numbers.push (43)
+# removes first array
+tax_returns.shift
+# adds to start of array
+full_time_student.unshift (true)
